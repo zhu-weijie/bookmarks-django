4a990dd51728d5fc3975bace8a56cc26c2837bd4
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2023-03-06 12:01:08 +0800

    ✨ feat: Added tags to Bookmark model

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class Bookmark(models.Model):
@@ -14,6 +15,7 @@ class Bookmark(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
