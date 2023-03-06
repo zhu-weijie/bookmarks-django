07375f51973f9f23dbac0f049320bf69548b65b9
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2023-03-06 10:01:23 +0800

    ✨ feat: Add bookmark model to the admin site

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Bookmark
 
-# Register your models here.
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'link']
+    list_filter = ['collector', 'publish', 'created', 'updated']
+    search_fields = ['name', 'link']
+    ordering = ['-publish']
