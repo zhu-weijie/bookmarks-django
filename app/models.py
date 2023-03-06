5b3bad7387a9dd39377073f6ec25b8a4af8a51b1
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2023-03-06 09:36:38 +0800

    ✨ feat: each bookmark collected by a user, a user can collect many bookmarks

--- app/models.py
+++ app/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Bookmark(models.Model):
     name = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     link = models.CharField(max_length=250)
+    collector = models.ForeignKey(User,
+                                  on_delete=models.CASCADE,
+                                  related_name='bookmarks')
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
