f6252e71811e7c0153e8fa5a424f39f27eb4fd93
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2023-03-06 11:46:01 +0800

    ✨ feat: Added pagination

--- app/views.py
+++ app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from .models import Bookmark
 
 
 def bookmark_list(request):
-    bookmarks = Bookmark.objects.all()
+    bookmark_list = Bookmark.objects.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(bookmark_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        bookmarks = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        bookmarks = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        bookmarks = paginator.page(paginator.num_pages)
     return render(request, 'app/bookmark/list.html', {'bookmarks': bookmarks})
 
 
